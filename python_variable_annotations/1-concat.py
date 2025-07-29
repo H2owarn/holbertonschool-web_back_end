d811130b596df375af76a1b312a3f0732c156f25
Wawa <chutaphat@hotmail.com>
2025-07-29 15:21:28 +1000

    returns a concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that provides a single function to returns a concatenated string.
+
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Add two string and return concatenated string.
+
+    Args:
+        a (str): The first string.
+        b (str): The second string.
+
+    Returns:
+        string: The result of concatenated string  a and b.
+    """
+    return str1 + str2
