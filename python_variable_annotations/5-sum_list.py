3a50e85424ed32fa3c75b255fb9bfc086b04d310
Wawa <chutaphat@hotmail.com>
2025-07-30 21:47:54 +1000

    Fix: from typing import List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,10 +4,12 @@ Module takes a list mxd_lst of integers
 and floats and returns their sum as a float.
 """
 
-def sum_list(input_list: float) -> float:
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
     """Return sum
         Args:
-        input_list (flat): The input list.
+        input_list list[float]: The input list.
 
     Returns:
         sum
