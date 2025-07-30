56da74ee422acae0a76d1258a9eb64c1ea65850c
Wawa <chutaphat@hotmail.com>
2025-07-30 19:00:27 +1000

    take float return floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import math
+"""
+Module that provides function floor which takes a float n as argument and returns the floor of the float.
+
+"""
+
+
+def floor(n: float) -> int:
+    """Add one float return int.
+
+    Args:
+        ans (float): The flaot
+
+    Returns:
+        int: the floor of the float.
+    """
+    return math.floor(n)
