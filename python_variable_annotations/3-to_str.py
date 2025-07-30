68d199428a97410d203d019749ee06cee9434b13
Wawa <chutaphat@hotmail.com>
2025-07-30 19:31:18 +1000

    take number return str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module that takes a float n as argument and
+returns the string representation of the float.
+"""
+
+import math
+
+
+def to_str(n: float) -> str:
+    """Return the string of the float.
+
+    Args:
+        n (float): The input number.
+
+    Returns:
+        str: the string representation of the float.
+    """
+    return str(n)
