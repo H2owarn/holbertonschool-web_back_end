44814c368ec98719f403865556320797b016a6c4
Wawa <chutaphat@hotmail.com>
2025-07-30 21:44:18 +1000

    sum list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module takes a list mxd_lst of integers
+and floats and returns their sum as a float.
+"""
+
+def sum_list(input_list: float) -> float:
+    """Return sum
+        Args:
+        input_list (flat): The input list.
+
+    Returns:
+        sum
+    """
+    return sum(input_list)
