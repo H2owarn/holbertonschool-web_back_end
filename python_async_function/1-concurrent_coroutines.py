40983c5519ef6d7104ad1340c91480cef9c248fa
Wawa <chutaphat@hotmail.com>
2025-07-31 12:37:40 +1000

    multiple coroutines at the same time

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
 
+"""
+ execute multiple coroutines at the same time with async
+
+"""
 
 import heapq
 import asyncio
 from typing import List
 
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    heap = []
+    """The list of the delays
+        Args:
+        n [int]: The input number.
+        mxd_lst[int]: default 10
+
+    Returns:
+        The list of the delays
+    """
+
+    heap = []   # pop them in order
 
     async def track_delay():
         delay = await wait_random(max_delay)
-        heapq.heappush(heap, delay)
+        heapq.heappush(heap, delay)  # collect results out of order
 
     await asyncio.gather(*(track_delay() for _ in range(n)))
     return [heapq.heappop(heap) for _ in range(len(heap))]
-
-
