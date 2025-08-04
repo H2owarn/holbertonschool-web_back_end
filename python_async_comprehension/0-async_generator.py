7dbc5cec06ffd869ba32241df8b8fe192d1612c7
Wawa <chutaphat@hotmail.com>
2025-08-04 11:23:44 +1000

    Fix return value to float

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+"""
+This module asynchronous generator coroutine
+that loops 10 times, waits 1 second each time,
+and yields a random number between 0 and 10
+"""
 
 import asyncio
 import random
 from typing import AsyncGenerator
 
-async def async_generator() -> AsyncGenerator[int, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Return random float number
+    Args:
+    ()
+    Return:
+    Random [float] between 1 to 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(1, 10)
+        yield random.uniform(1.00, 10.00)
