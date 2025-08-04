3648b9593449a3d0c154153219d012ad6c063973
Wawa <chutaphat@hotmail.com>
2025-08-04 11:50:22 +1000

    Return random number over async_genrator

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+This module that takes no arguments.
+The coroutine will collect 10 random numbers
+using an async comprehensing over async_generator,
+then return the 10 random numbers.
+"""
+
+import asyncio
+import random
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    """Return 10 random number
+    Args:
+    ()
+    Return:
+    return the 10 random numbers
+    over async_generator
+    """
+    for _ in range(async_generator):
+        yield random.uniform(0, 10)
