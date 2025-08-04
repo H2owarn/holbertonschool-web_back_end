054def06376ef3cdb5ee5a1a2420ba3d5dde744e
Wawa <chutaphat@hotmail.com>
2025-08-04 13:05:44 +1000

    Fix:style

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,11 +6,12 @@ write a measure_runtime coroutine that will
 execute async_comprehension four times
 in parallel using asyncio.gather.
 """
+import asyncio
+import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-import asyncio
-import time
+
 
 
 async def measure_runtime() -> float:
@@ -21,13 +22,10 @@ async def measure_runtime() -> float:
     """
     start_time = time.perf_counter()
 
-
     # Run async_comprehension four times
     await asyncio.gather(async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+        async_comprehension(), async_comprehension(),
+        async_comprehension())
 
     end_time = time.perf_counter()
     return end_time - start_time
