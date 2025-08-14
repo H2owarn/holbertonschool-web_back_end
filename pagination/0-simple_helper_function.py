99db7e2d713cdf2773be7448ed5e1066427545ad
Wawa <chutaphat@hotmail.com>
2025-08-14 13:50:44 +1000

    code style

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """
-This module takes two integer arguments page and page_size. and return a tuple of size two containing a start index and an end index corresponding
+This module takes two integer arguments page
+and page_size. and return a tuple of
+size two containing a start index and
+an end index corresponding
 
 """
+
 
 def index_range(page: int, page_size: int) -> tuple:
     """ Return a tuple
@@ -16,4 +20,3 @@ def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
-
