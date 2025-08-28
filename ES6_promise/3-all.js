68c99e1893f37b87ff820734535363dcd9cec464
Wawa <chutaphat@hotmail.com>
2025-08-28 17:38:55 +1000

    invoke inside the array

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import {uploadPhoto, createUser} from "./utils.js";
+
+
+export default function handleProfileSignup() {
+    return Promise.all([uploadPhoto(), createUser()])
+        .then(([Photores, Userres]) => {
+            const { body } = Photores;
+            const { firstName, lastName } = Userres;
+            console.log(`${body}, ${firstName}, ${lastName}`);
+        })
+        .catch(error => {
+            console.error('failed', error)
+        });
+}
