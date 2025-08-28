5a309e02c6368394b51056a9a2fb87552d807191
Wawa <chutaphat@hotmail.com>
2025-08-28 14:14:02 +1000

    remove perfix and join with '-'

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet (set, startString) {
+    if (!startString || typeof startString != 'string')
+        return '';
+
+    return [...set]
+    .filter(str => str.startsWith(startString))
+    .map(str => str.slice(startString.length))
+    .join('-');
+}
