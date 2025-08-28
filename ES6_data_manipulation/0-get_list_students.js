2a556c6033e087107c2a391aeaa03a5ec4ccff86
Wawa <chutaphat@hotmail.com>
2025-08-28 11:07:47 +1000

    return an array

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+    return [
+        {id: 1, firstnme: 'Guillaume', location: 'San Francisco'},
+        {id: 2, firstnme: 'James', location: 'Columbia'},
+        {id: 5, firstnme: 'Serena', location: 'San Francisco'}
+    ];
+}
