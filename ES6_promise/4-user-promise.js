d5b1246d12561152c2265a814f2bbc38d92f5a5e
Wawa <chutaphat@hotmail.com>
2025-08-28 19:13:32 +1000

    returns a real JavaScript object

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,10 @@
+export default function signUpUser(firstName, lastName) {
+    return new Promise((resolve, reject) => {
+        const success = true;
+        if (success) {
+            resolve({ firstName, lastName });
+        } else {
+            reject('Failed');
+        }
+    });
+}
