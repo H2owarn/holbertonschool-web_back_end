480e10269159b68470a7ce116e918bd16a61f7f7
Wawa <chutaphat@hotmail.com>
2025-08-30 21:40:43 +1000

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,22 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup (firstName, lastName, fileName) {
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName),
+    ]).then((results) => {
+        return results.map((result) => {
+            if (result.status == 'fulfilled') {
+            return {
+                status: 'fulfilled',
+                value: results.value,
+            };
+        }
+        return {
+            status: 'rejected',
+            value: results.reason,
+        };
+      });
+    });
+}
