0b25b96378b8eb94ced4184ad38c3241aa02156d
Wawa <chutaphat@hotmail.com>
2025-09-04 13:16:14 +1000

    take input

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdin.setEncoding('utf8');
 
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (input) => {
   const INPUT = input.trim();
-  console.log(`Your name is: ${INPUT}\n`);
+  console.log(`Your name is: ${INPUT}`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
