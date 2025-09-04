6030e3ec94e128d78ef3e7e614d01a7078f1a207
Wawa <chutaphat@hotmail.com>
2025-09-04 16:56:58 +1000

    readFile (Asynchronous)

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return fs.promises.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const headers = lines[0].split(',');
+      const fieldIndex = headers.indexOf('field');
+      const firstNameIndex = headers.indexOf('firstname');
+
+      const studentsByFild = {};
+
+      for (let i = 1; i < lines.length; i += 1) {
+        const values = lines[i].split(',');
+        const field = values[fieldIndex];
+        const firstName = values[firstNameIndex];
+
+        if (!studentsByFild[field]) {
+          studentsByFild[field] = [];
+        }
+        studentsByFild[field].push(firstName);
+      }
+
+      const totalStudents = Object.values(studentsByFild)
+        .reduce((acc, curr) => acc + curr.length, 0);
+
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [field, names] of Object.entries(studentsByFild)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
