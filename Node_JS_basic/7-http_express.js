dc2e5815cdabe4cd6c841e1a3b7b551a46781970
Wawa <chutaphat@hotmail.com>
2025-09-07 16:57:16 +1000

    HTTP server using express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+
+const countStudents = require('./3-read_file_async');
+
+const databasePath = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(databasePath)
+    .then((output) => {
+      res.send(`This is the list of our students\n${output}`);
+    });
+});
+
+app.listen(PORT);
+
+module.exports = app;
